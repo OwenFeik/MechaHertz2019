9ea83488ce32f37284e0c3f7100bc200e14e37d3
OwenFeik <owenhfeik@gmail.com>
2019-02-19 19:20:11 +1100

    Refactored for readability

--- draw_lidar.py
+++ draw_lidar.py
@@ -9,34 +9,45 @@ data=lidar.iter_scans()
 # with open('dummy2.json','r') as f:
 #     data=json.load(f)
 
+def make_point(bearing,distance):
+    """Bearing is the bearing of the lidar reading in degrees, distance is the distance in mm.\nReturns a (x,y) point."""
+    bearing=radians(bearing)
+
+    x=int((sin(bearing)*distance)/4)+500
+    y=int(-1*((cos(bearing)*distance)/4))+500
+    
+    return (x,y)
+
+def in_range(point_a,point_b,dist):
+    """Points a and b are two (x,y) coordinates, dist is the maximum allowable distance betwee them.\nReturns True or False based on pythagorean distance."""
+
+    a=max(point_a[0],point_b[0])-min(point_a[0],point_b[0])
+    b=max(point_a[1],point_b[1])-min(point_a[1],point_b[1])
+
+    if sqrt(a**2+b**2)<dist:
+        return True
+    return False
+
+
+colour=(0,255,0) # Draw colour
+max_dist=20 # Max distance to consider 2 points of the same object
+
 # Scan looks like this:
 # [(quality, bearing, distance), (12, 345.25, 450)]
 
 prev=(0,0)
 for scan in data:
     base=cv2.imread('base.png')
-    for point in scan:
-        bearing=radians(point[1])
-        distance=point[2]
+    for reading in scan:
 
-        x=int((sin(bearing)*distance)/4)+500
-        y=int(-1*((cos(bearing)*distance)/4))+500
+        point=make_point(reading[1],reading[2])
+
+        cv2.circle(base,point,2,colour,2)
         
+        if in_range(point,prev,max_dist):
+            cv2.line(base,point,prev,colour,2)            
 
-        if radians(20)>bearing or bearing>radians(340):
-            colour=(0,0,255)
-        else:
-            colour=(0,255,0)
-
-        cv2.circle(base,(x,y),2,colour,2)
-
-        a=max(prev[0],x)-min(prev[0],x)
-        b=max(prev[1],y)-min(prev[1],y)
-
-        if sqrt((a**2)+(b**2))<20:
-            cv2.line(base,(x,y),prev,colour,2)            
-
-        prev=(x,y)
+        prev=point
 
 
     cv2.imshow('scan',base)
