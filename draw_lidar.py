157c1789d4bdb8e9865dd46bf556707c787bef74
OwenFeik <owenhfeik@gmail.com>
2019-02-24 16:40:39 +1100

    Frequency based edge tracking

--- draw_lidar.py
+++ draw_lidar.py
@@ -2,160 +2,101 @@ from rplidar import RPLidar
 from math import sin,cos,radians,sqrt
 import cv2
 
-# lidar=RPLidar('COM3')
-# data=lidar.iter_scans()
+lidar=RPLidar('COM3')
+data=lidar.iter_scans()
 
-import json # Use dummy data for no lidar
-with open('datastills.json','r') as f:
-    data=json.load(f)
+# import json # Use dummy data for no lidar
+# with open('data.json','r') as f:
+#     data=json.load(f)
 
 def make_point(bearing,distance):
     """Bearing is the bearing of the lidar reading in degrees, distance is the distance in mm.\nReturns an (x,y) point."""
     bearing=radians(bearing)
 
-    x=int((sin(bearing)*distance)/4)+500
-    y=int(-1*((cos(bearing)*distance))/4)+500
+    x=int((sin(bearing)*distance)/4)
+    y=int(-1*((cos(bearing)*distance))/4)
     
-    return Point(x,y)
+    return (x,y)
 
-def in_range(point_a,point_b,dist):
-    """Points a and b are two (x,y) coordinates, dist is the maximum allowable distance betwee them.\nReturns True or False based on pythagorean distance."""
-
-    a=max(point_a[0],point_b[0])-min(point_a[0],point_b[0])
-    b=max(point_a[1],point_b[1])-min(point_a[1],point_b[1])
-
-    if sqrt(a**2+b**2)<dist:
-        return True
-    return False
-
-class Point():
-    def __init__(self,x,y,alt=None):
-        self.x=x
-        self.y=y
-        self.alt=alt
-
-    @property
-    def point(self):
-        return (self.x,self.y)
-
-
-class Line(): # Series of points with similar x or y values
-    def __init__(self,direction,points=None):
-        self.direction=direction
-        self.points=points if points else []            
-
-    def __str__(self):
-        return str(self.points)
-
-    def add_point(self,point):
-        if abs(point.x-self.points[0].x)>abs(point.y-self.points[0].y):
-            self.direction='h'
-        else:
-            self.direction='v'
-        self.points.append(point)
-
-    def draw_self(self,img):
-        if self.direction=='v' and self.points[len(self.points)-1].x-self.points[0].x>20:
-            return img
-        elif self.direction=='h' and self.points[len(self.points)-1].y-self.points[0].y>20:
-            return img
-
-        if self.direction=='h':
-            colour=(255,0,0)
-        else:
-            colour=(0,0,255)
-
-        for i in range(1,len(self.points)):
-            cv2.line(img,self.points[i].alt,self.points[i-1].alt,colour,2)
-
-        return img
-
-class Shape(): # Cloud of nearby points for analysis
+class Map():
     def __init__(self,points=None):
         self.points=points if points else []
 
-    def find_lines(self):
-        _lines=[]
-        line=None
+    def find_edges(self):
+        top_c=0
+        top_y=0
+        for i in range(1,500,10):
+            count=0
+            for p in self.points:
+                if abs(p[1]-i)<10:
+                    count+=1*i
 
-        _prev=None
-        for _point in self.points:
-            if _prev:
-                if line:
-                    if line.direction=='v':
-                        if abs(_point.x-_prev.x)<=5:
-                            line.add_point(_point)
-                            # line.points.append(_point)
-                    elif line.direction=='h':
-                        if abs(_point.y-_prev.y)<=5:
-                            line.add_point(_point)
-                            # line.points.append(_point)
-                elif abs(_point.x-_prev.x)<=5:
-                    if line and line.direction=='v':
-                        line.add_point(_point)
-                        # line.points.append(_point)
-                    elif line:
-                        _lines.append(line)
-                        line=Line('v',[_prev,_point])
-                    else:
-                        line=Line('v',[_prev,_point])
-                elif abs(_point.y-_prev.y)<=5:
-                    if line and line.direction=='h':
-                        line.add_point(_point)
-                        # line.points.append(_point)
-                    elif line:
-                        _lines.append(line)
-                        line=Line('h',[_prev,_point])
-                    else:
-                        line=Line('h',[_prev,_point])
-            _prev=_point
-        if line:
-            _lines.append(line)
+            if count>top_c:
+                top_c=count
+                top_y=i
+        
+        bot_c=0
+        bot_y=0
+        for i in range(-500,0,10):
+            count=0
+            for p in self.points:
+                if abs(p[1]-i)<10:
+                    count+=1*-i
 
-        return _lines
+            if count>bot_c:
+                bot_c=count
+                bot_y=i
 
-colour=(0,255,0) # Point draw colour
+        lft_c=0
+        lft_x=0
+        for i in range(-500,0,10):
+            count=0
+            for p in self.points:
+                if abs(p[0]-i)<10:
+                    count+=1*-i
+
+            if count>lft_c:
+                lft_c=count
+                lft_x=i
+
+        rgt_c=0
+        rgt_x=0
+        for i in range(1,500,10):
+            count=0
+            for p in self.points:
+                if abs(p[0]-i)<10:
+                    count+=1*i
+
+            if count>rgt_c:
+                rgt_c=count
+                rgt_x=i
+
+        return lft_x,rgt_x,top_y,bot_y
+
+point_colour=(0,255,0)
+line_colour=(0,0,255)
 max_dist=50 # Max distance to consider 2 points of the same object
+angle=-2
 
 # Scan looks like this:
 # [(quality, bearing, distance), (12, 345.25, 450)]
 
 go=True
 while go:
-    prev=make_point(0,0)
     for scan in data:
         base=cv2.imread('base.png')
-        shapes=[]
-        shape=None
+        m=Map()
         for reading in scan:
+            point=make_point(reading[1]+angle,reading[2])
+            m.points.append(point)
 
-            point=make_point(reading[1],reading[2]).point
-            r_point=make_point(reading[1],reading[2])
-            r_point.alt=point
+            cv2.circle(base,(point[0]+500,point[1]+500),2,point_colour,2)
 
-            cv2.circle(base,point,2,colour,2)
-            
-            if in_range(r_point.point,prev.point,max_dist):
-                if shape:
-                    shape.points.append(r_point)
-                else:
-                    shape=Shape([prev,r_point])
-            else:
-                if shape:
-                    shapes.append(shape)
-                shape=None
-
-            prev=r_point
-
-        if shape:
-            shapes.append(shape)
-
-        lines=[]
-        for shape in shapes:
-            lines.extend(shape.find_lines())
-
-        for line in lines:
-            base=line.draw_self(base)
+        l,r,t,b=m.find_edges()
+        cv2.line(base,(l+500,0),(l+500,1000),line_colour)
+        cv2.line(base,(r+500,0),(r+500,1000),line_colour)
+        cv2.line(base,(0,t+500),(1000,t+500),line_colour)
+        cv2.line(base,(0,b+500),(1000,b+500),line_colour)
 
         cv2.imshow('scan',base)
         key=cv2.waitKey(30) & 0xFF
