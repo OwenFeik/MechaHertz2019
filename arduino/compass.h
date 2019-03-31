652d1771ccbc833219e3f82f52c60f0adf0c2f6a
OwenFeik <owenhfeik@gmail.com>
2019-03-31 23:17:57 +1100

    modifying compass to allow calibration

--- arduino/compass.h
+++ arduino/compass.h
@@ -1,13 +1,16 @@
 #include <Wire.h> // Library to interface with the magnetometer
 
-class Compass
-{
-    private:
-        float x_offset;
-        float y_offset;
-    
+class Compass{
     public:
-    
+        float x_offset,y_offset;
+        int x,y,z;
+        float heading;
+
+    void calibrateSelf();
+    void updateXYZ();
+    float getHeading();
+    float getAccurateHeading();
+
     Compass(){
         Wire.begin();
         Wire.beginTransmission(0x1E); // Set up the magnetometer
@@ -15,38 +18,57 @@ class Compass
         Wire.write(0x00);
         Wire.endTransmission();
     }
-
-    float getHeading();
-    float getAccurateHeading();
 };
 
 
-// Get current bearing of compass relative to T
-float Compass::getHeading(){  
+// Calculate the x and y offset of the compass (doesn't work atm)
+void Compass::calibrateSelf(){
+    int min_x=this->x,min_y=this->y,max_x=this->x,max_y=this->y;
+
+    for (int i=0;i<100;i++){
+        this->updateXYZ();
+        if (this->x>max_x){max_x=this->x;}
+        if (this->y>max_y){max_y=this->y;}
+        if (this->x<min_x){min_x=this->x;}
+        if (this->y<min_x){min_x=this->y;}
+        delay(100);
+    }
+
+    this->x_offset=(min_x+max_x)/2;
+    this->y_offset=(min_y+max_y)/2;
+
+    Serial.println(x_offset);
+    Serial.println(y_offset);
+}
+
+
+void Compass::updateXYZ(){
     Wire.beginTransmission(0x1E);
     Wire.write(0x03); //select register 3, X MSB register
     Wire.endTransmission();
 
-    int x,y,z;
-
     Wire.requestFrom(0x1E, 6);
     if(6<=Wire.available()){
-        x = Wire.read()<<8; //X msb
-        x |= Wire.read(); //X lsb
-        z = Wire.read()<<8; //Z msb
-        z |= Wire.read(); //Z lsb
-        y = Wire.read()<<8; //Y msb
-        y |= Wire.read(); //Y lsb
+        this->x = (Wire.read()<<8 | Wire.read());
+        this->z = Wire.read()<<8 | Wire.read();
+        this->y = (Wire.read()<<8 | Wire.read());
     }
+}
+
+// Get current bearing of compass relative to T
+float Compass::getHeading(){  
+
+    this->updateXYZ();
 
     float heading;
 
-    heading=atan2(x, y)/0.0174532925;
+    heading=atan2(this->x, this->y)/0.0174532925;
     if (heading<0){
         heading+=360;
     }
-    heading=abs(heading-360);
-    return heading;
+    this->heading=abs(heading-360);
+
+    return this->heading;
 }
 
 // Return the average of 10 compass readings
@@ -55,5 +77,7 @@ float Compass::getAccurateHeading(){
     for(int i=0; i<10; i++){
         heading_sum+=getHeading();
     }
-    return heading_sum/10;
+    this->heading=heading_sum/10;
+
+    return this->heading;
 }
